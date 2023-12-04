96ad4d1ea9d9d2d1327043ce97b1e30ed6df6120
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-04 22:40:45 +0200

    Add files via upload

---
+++ color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// theme colors
+    Color blackPrimary = Color(0xff212131);
+    Color black = Color(0xff09050D);
+    Color grey = Color(0xffc8c3e8);
+    Color white = Color(0xffffffff);
+    Color purple = Color(0xff6d48ff);
+    Color softPurple = Color(0xffD0C3FF);
+    Color pink = Color(0xfff85c96);
+    Color cyan = Color (0xff6ab0c1);
+    Color orange = Color(0xffff9e52);
