e7ba5941ad6ba13140f5b9bc96ac891108a8cc00
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-05 21:05:59 +0200

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,81 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'budget_setting.dart';
+import 'budget_view.dart';
+import "color.dart';
+import 'custom_widgets.dart';
 import 'expense_view.dart';
 import 'expense_entry.dart';
-import 'budget_view.dart';
+
+
+
 
 class HomePage extends StatelessWidget {
   final UserCredential userCredential;
 
   HomePage(this.userCredential);
 
-  @override
+ @override
   Widget build(BuildContext context) {
     String email = userCredential.user?.email ?? '';
-    //String userName = email.split('@')[0];
+    String userName = email.split('@')[0];
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+          title: const Text('Home'),
+          centerTitle: true,
+          backgroundColor: cyan,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              )
+          )
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body:
+        Column(
           children: [
-            ElevatedButton(
-              child: Text('Enter New Expenses'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ExpenseEntryPage(),
-                    settings: RouteSettings(
-                      arguments: userCredential,
+            Align(
+              alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/logo3.png',
+                  width: 100,
+                  fit: BoxFit.contain,
+                ),
+            ),
+
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                  ElevatedButton(
+                  style: buttonPrimary,
+                    child: const Text('Enter New Expenses'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ExpenseEntryPage(),
+                          settings: RouteSettings(
+                            arguments: userCredential,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: buttonPrimary,
+                      child:  const Text('View Expenses'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ExpenseViewPage(userID: userCredential.user?.uid ?? ''),
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text('View Expenses'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ExpenseViewPage(userID: userCredential.user?.uid ?? ''),
-                  ),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text('Set a New Budget'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BudgetSettingsPage(userId: userCredential.user?.uid ?? ''),
-                    settings: RouteSettings(
-                      arguments: userCredential,
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: buttonPrimary,
+                      child: const Text('Set a New Budget'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BudgetSettingsPage(userId: userCredential.user?.uid ?? ''),
+                            settings: RouteSettings(
+                              arguments: userCredential,
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text('View Budgets'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BudgetViewPage(userId: userCredential.user?.uid ?? ''),
-                  ),
-                );
-              },
-            ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: buttonPrimary,
+                      child: const Text('View Budgets'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BudgetViewPage(),
+                            settings: RouteSettings(
+                              arguments: userCredential,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
-      ),
     );
   }
 }
