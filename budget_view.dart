e57bdfcc66a999d97606bdccee1570f5f91f0293
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-05 23:30:46 +0200

    Update budget_view.dart UI

--- budget_view.dart
+++ budget_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'color.dart';
+
 
 class BudgetViewPage extends StatefulWidget {
-  final String userId;
-
-  BudgetViewPage({required this.userId});
-
   @override
   _BudgetViewPageState createState() => _BudgetViewPageState();
 }
@@ -23,7 +21,7 @@ class _BudgetViewPageState extends State<BudgetViewPage> {
   void fetchMonthlyBudget() async {
     DocumentSnapshot snapshot = await FirebaseFirestore.instance
         .collection('budgetSettings')
-        .doc(widget.userId)
+        .doc('userSettings')
         .get();
 
     if (snapshot.exists) {
@@ -31,58 +29,78 @@ class _BudgetViewPageState extends State<BudgetViewPage> {
         Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
         monthlyBudget = data['monthlyBudget'] ?? 0.0;
       });
-      calculateRemainingBudget();
+
+      calculateRemainingBudget(); // Call calculateRemainingBudget here
     }
   }
 
   void calculateRemainingBudget() async {
-    DateTime now = DateTime.now();
-    DateTime startOfMonth = DateTime(now.year, now.month, 1);
-    DateTime endOfMonth = DateTime(now.year, now.month + 1, 0, 23, 59, 59);
-
     QuerySnapshot expenseSnapshot = await FirebaseFirestore.instance
         .collection('expenses')
-        .doc(widget.userId)
-        .collection('userExpenses')
-        .where('dateTime', isGreaterThanOrEqualTo: startOfMonth)
-        .where('dateTime', isLessThanOrEqualTo: endOfMonth)
+        .where('month', isEqualTo: DateTime.now().month)
         .get();
 
-    if (expenseSnapshot.docs.isNotEmpty) {
-      double totalExpenses = 0.0;
+    double totalExpenses = 0.0;
 
-      for (var expense in expenseSnapshot.docs) {
-        totalExpenses += expense.get('amount') ?? 0.0;
-      }
+    expenseSnapshot.docs.forEach((expense) {
+      totalExpenses += expense.get('amount') ?? 0.0;
+    });
 
-      setState(() {
-        remainingBudget = monthlyBudget - totalExpenses;
-      });
-    }
+    setState(() {
+      remainingBudget = monthlyBudget - totalExpenses;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Budget View'),
+          title: const Text('View budget'),
+          centerTitle: true,
+          backgroundColor: cyan,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              )
+          )
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Monthly Budget: ${monthlyBudget.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Remaining Budget: ${remainingBudget.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
-      ),
+       body: Padding(
+         padding: EdgeInsets.all(16.0),
+         child: Column(
+           children: [
+             Align(
+               alignment: Alignment.topLeft,
+               child: Image.asset(
+                 'assets/images/logo3.png',
+                 width: 100,
+                 fit: BoxFit.contain,
+               ),
+             ),
+           Center(
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Text(
+                     'Monthly Budget: ${monthlyBudget.toStringAsFixed(2)}',
+                     style: TextStyle(fontSize: 24,
+                         color: cyan),
+                   ),
+                   SizedBox(height: 16),
+                   Text(
+                     'Remaining Budget: ${remainingBudget.toStringAsFixed(2)}',
+                     style: TextStyle(fontSize: 24,
+                         color: cyan
+                     ),
+                   ),
+                 ],
+               ),
+             ),
+           ],
+         )
+       )
+  
     );
   }
 }
+
