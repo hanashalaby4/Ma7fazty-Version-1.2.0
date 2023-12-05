1f1dcd39163f7219688d3a3d94e6de11186526be
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-05 21:01:17 +0200

    Changed ThemeData

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart'; //Import statement to import the firebase_core package that initialized firbase in the app
 import 'package:flutter/material.dart'; //import material package (design widgets)
 import 'signup.dart';
+import 'color.dart';
 import 'firebase_options.dart'; //import a dart file that contains the details needed to establish a connection between Ma7fazti and firebase services
 
 
@@ -21,21 +22,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ma7fazti',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        textTheme:
+        TextTheme(titleMedium: TextStyle(color: softPurple),
+        ),
+      scaffoldBackgroundColor: const Color(0xff212131),
+        primaryColor: const Color(0xffCBCCD5),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
