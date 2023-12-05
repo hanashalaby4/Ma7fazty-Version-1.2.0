0b4a0f224e1e77177228a96ff7c25567186b3038
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-05 23:47:09 +0200

    Update color.dart

--- color.dart
+++ color.dart
@@ -5,7 +5,23 @@ import 'package:flutter/material.dart';
     Color grey = Color(0xffc8c3e8);
     Color white = Color(0xffffffff);
     Color purple = Color(0xff6d48ff);
-    Color softPurple = Color(0xffD0C3FF);
+
     Color pink = Color(0xfff85c96);
-    Color cyan = Color (0xff6ab0c1);
+    Color cyan = Color (0xff63adbd);
     Color orange = Color(0xffff9e52);
+
+    Color softPurple = Color(0xffD0C3FF);
+    Color softPink = Color(0xfffda1c1);
+    Color softOrange = Color(0xfffdba87);
+    Color softCyan = Color(0xff63adbd);
+    Color softGreen = Color(0xff90fabc);
+    Color softYellow = Color(0xffeffa90);
+
+    List<Color> customColors = [
+        softPurple,
+        softCyan,
+        softOrange,
+        softPink,
+        softGreen,
+        softYellow,
+    ];
